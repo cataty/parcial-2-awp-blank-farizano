175fdbe53c66180d2fc0c09e1cf9a3ec5129a245
cataty <tatjana.blank@yahoo.de>
2024-06-13 16:18:22 -0300

    feat: traer detalles de la planta por id y mostrarlas en la página 'plant', agregar placeholder a la lista

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,19 @@ const getEdiblePlants = async () => {
 }
 
 const getPlant = async (id) => {
-    await fetch(`https://perenual.com/api/species/details/${id}?${token}`)
-    .then(response => response.json())
-    .then(response => response.data.forEach(plant => {
-        renderCard(plant);
-    }))
+    await fetch(`https://perenual.com/api/species/details/${id}?key=${token}`)
+    .then(response => {
+        if (response.status === 200) {
+            response.json()
+            .then(response => renderDetails(response));
+        } else if (response.status === 404) {
+            create404(plant);
+        };
+    })
     .catch(error => console.log('error', error));
 }
+
+const create404 = () => {} // TODO: crear 404
 
 const renderCard = (plant, containerID) => {
     console.log(plant);
@@ -25,7 +31,7 @@ const renderCard = (plant, containerID) => {
         <div class="col s12 m6 l4">
             <div class="card">
                 <div class="card-image">
-                    <img src='${plant.default_image.small_url}'>
+                    <img src='${(plant.default_image.small_url ? plant.default_image.small_url : '/img/placeholder.png')} '>
                     <span class="card-title">${plant.common_name}</span>
                 </div>
                 <div class="card-content">
@@ -35,7 +41,7 @@ const renderCard = (plant, containerID) => {
                     <p>cycle: ${plant.cycle}</p>
                 </div>
                 <div class="card-action">
-                    <a href="/plant.html?${plant.id}">Learn more</a>
+                    <a class="green-text text-accent-4" href="plant.html?id=${plant.id}">Learn more</a>
                 </div>
             </div>
         </div>
@@ -43,8 +49,52 @@ const renderCard = (plant, containerID) => {
     plantsContainer.innerHTML += plantCard;
 }
 
-const renderDetail = (plant) => {
-
+const renderDetails = (plant) => {
+    console.log(plant);
+    const plantContainer = document.getElementById("plantDetailContainer");
+    const plantCard = `
+        <div class="col s12 m12 l12">
+            <div class="card">
+                    <div class="card-image">
+          <img src="${plant.default_image.regular_url}" alt="${plant.name}">
+          <span class="card-title">${plant.name}</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light green-accent-2"><i class="material-icons">add</i></a>
+                <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-tabs">
+                <ul class="tabs tabs-fixed-width">
+                    <li class="tab"><a href="#test4">Test 1</a></li>
+                    <li class="tab"><a class="active" href="#test5">Test 2</a></li>
+                    <li class="tab"><a href="#test6">Test 3</a></li>
+                </ul>
+                </div>
+                <div class="card-content grey lighten-4">
+                <div id="test4">Test 1</div>
+                <div id="test5">
+                    <h3>Growing information<h3>
+                    <h4>Watering</h4>
+                    <ul>
+                        <li>frequency: ${plant.watering}</li>
+                        <li>time period:  ${plant.watering_period}</li>
+                        <li>recomendation: ${plant.watering_general_benchmark.value} ${plant.watering_general_benchmark.unit} </li>
+                        <li>watering depth: ${plant.depth_water_requirement.value} ${plant.depth_water_requirement.unit}</li>
+                    </ul>
+                </div>
+                <div id="test6">Test 3</div>
+                </div>
+            </div>
+        </div>
+    `
+    plantContainer.innerHTML += plantCard;
 }
 
-getEdiblePlants();
+
+window.addEventListener("DOMContentLoaded", (e) => {
+    if (window.location.href.includes('index.html')){
+        getEdiblePlants();
+    } else {
+        const searchParams = new URLSearchParams(window.location.search);
+        getPlant(searchParams.get('id'));
+    }
+});
