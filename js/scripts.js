6e057749b79c6114e09c2784ff8047adbf7d59dc
cataty <tatjana.blank@yahoo.de>
2024-06-13 14:08:19 -0300

    feat: conectar con perenual API y traer plantas en cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,50 @@
-let token;
+const token = "sk-cIK4666b199c48f9f5915";
 
-const auth = () => {
-    fetch("http://trefle.io/api/auth/claim", {
-        method: "POST",
-        body: JSON.stringify({
-                "origin": "http://127.0.0.1:5500/index.html",
-                "ip": "190.229.159.80",
-                "token": "VyAG9-mhukFFwjlRaB0P-OM2eNyEiNP5inutaNpx2Bw"
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-      }})
-      .then(response => response.json)
-      .then(data => {
-            token = data.token;
-            console.log(token);
-      })
-    }
-
-
-
-const getPlants = () => {
-     fetch(`https://trefle.io/api/v1/plants?token=${token}`)
-     .then(response => response.json)
-     .then(data => console.log(data));
+const getEdiblePlants = async () => {
+    await fetch(`https://perenual.com/api/species-list?key=${token}&edible=1`)
+    .then(response => response.json())
+    .then(response => response.data.forEach(plant => {
+        renderCard(plant, "ediblePlantsContainer");
+    }))
+    .catch(error => console.log('error', error));
 }
 
-getPlants();
+const getPlant = async (id) => {
+    await fetch(`https://perenual.com/api/species/details/${id}?${token}`)
+    .then(response => response.json())
+    .then(response => response.data.forEach(plant => {
+        renderCard(plant);
+    }))
+    .catch(error => console.log('error', error));
+}
+
+const renderCard = (plant, containerID) => {
+    console.log(plant);
+    const plantsContainer = document.getElementById(containerID);
+    const plantCard = `
+        <div class="col s12 m6 l4">
+            <div class="card">
+                <div class="card-image">
+                    <img src='${plant.default_image.small_url}'>
+                    <span class="card-title">${plant.common_name}</span>
+                </div>
+                <div class="card-content">
+                    <p>${plant.scientific_name[0]}</p>
+                    <p>watering: ${plant.watering}</p>
+                    <p>sunlight: <span>${plant.sunlight[0]}</span><span>${plant.sunlight[1]}</span></p>
+                    <p>cycle: ${plant.cycle}</p>
+                </div>
+                <div class="card-action">
+                    <a href="/plant.html?${plant.id}">Learn more</a>
+                </div>
+            </div>
+        </div>
+    `
+    plantsContainer.innerHTML += plantCard;
+}
+
+const renderDetail = (plant) => {
+
+}
+
+getEdiblePlants();
